606cc1a1f65a1e874172a7d8ef839860f1053eac
NJ Development <nj.development83@gmail.com>
2026-01-01 02:47:09 -0300

    Initial Project

---
+++ MyIncome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyIncome
+//
+//  Created by NJ Development on 01/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyIncome/MyIncomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyIncomeApp.swift
+//  MyIncome
+//
+//  Created by NJ Development on 01/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MyIncomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
